6d3424007f52078a2f678e8c3035ddfda0555f58
Bithol Satapathy <bitholsatapathy2004@gmail.com>
2025-07-08 23:47:24 +0530

    day 1 project
    completed day 1 out of 100
    i made a band name generator in this .

---
+++ Day1/Band name generator.py
@@ -0,0 +1,5 @@
+print("Welcome to the band name generator!")
+city = input("what is the name of your city ?")
+pet = input("what is the name of ypur pet ?")
+band_name = city + " " + pet
+print("your band name is " + band_name)
---
+++ Day1/day1 stuff.py
@@ -0,0 +1,14 @@
+#printing stuff
+#print("Hello world!")
+#print("Hello world\nThis is a new line")
+#print("Hello"+" "+"world")
+# input("what is your name?")
+# print("Hello" + " " + input("what is your name?") + "!")
+# name = input("what is your name? ")
+# print("hello" + " " + name + "!" + "your name has" + " " + str(len(name)) + " " + "characters in it.")
+# username = input("what is your name?")
+# length = len(username)
+# print("hello " + username + " your name has " + str(length) + " letters.")
+user_name_of_the_person = "angela"
+length1 = len(user_name_of_the_person)
+print(length1)
