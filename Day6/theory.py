dda6c56f92c8d8f0b35548e50c49fb637830ec92
Bithol Satapathy <bitholsatapathy2004@gmail.com>
2025-09-07 20:10:41 +0530

    day 6
    function, while loop ,indentation

---
+++ Day6/theory.py
@@ -0,0 +1,35 @@
+#functions
+# print("Hello")
+# a = len("Hello")
+# print(a)
+
+# def my_function():
+#     print("Hello")
+#     print("Bye")
+    
+# my_function()
+
+# def my_function():
+#     #do this 
+#     #then do this
+#     #then do this
+
+# #indentation in python
+# def my_function():
+#     if sky == "clear":
+#         print("blue")
+#     elif sky == "cloudy":
+#         print("grey")
+#     print("Hello")
+# print("world")
+
+
+# #while loop
+# while some_Thing_is_true:
+#     #do that repeatedly
+
+#infinte loop
+while 5>3:
+    #do this
+    #do this
+    #do this
